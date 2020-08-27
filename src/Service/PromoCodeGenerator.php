988a7e6364d24ac961fdccf8767b800bbbfa8f6c
Mateusz Klencner <klencner97@gmail.com>
2020-08-27 02:52:56 +0200

    improved performance

--- src/Service/PromoCodeGenerator.php
+++ src/Service/PromoCodeGenerator.php
@@ -15,10 +15,10 @@ class PromoCodeGenerator
         $codes = [];
         for ($i1 = 0; $i1 < $amount; $i1++) {
             $characters = $alphanumeric ? '0123456789ABCDEFGHILKMNOPQRSTUVWXYZ' : '0123456789';
-            $charactersLength = strlen($characters);
+            $charactersMaxIndex = strlen($characters) - 1;
             $randomString = '';
             for ($i2 = 0; $i2 < $length; $i2++) {
-                $randomString .= $characters[rand(0, $charactersLength - 1)];
+                $randomString .= $characters[mt_rand(0, $charactersMaxIndex)];
             }
             $codes[] = $randomString;
         }
